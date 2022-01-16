852f13c34a37b95c7fa3cb0b95fedf5c57c8fe1f
AniaGorzkiewicz <228738@edu.p.lodz.pl>
2022-01-16 21:01:07 +0100

    1.4 accesory 2 getter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,6 +6,14 @@ public class DefaultCountingOutRhymer {
 
     private final int[] numbers = new int[ARRAY_ELEMENTS];
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     private int total = -1;
 
     public void countIn(int in) {
